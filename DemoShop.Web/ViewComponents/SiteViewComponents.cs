a1813f0c647dc3b9785c1792199e6d8bc02ff6bc
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-10-15 19:01:40 +0330

    10/15/2023 Section 8 Adding Header And Footer Components

---
+++ DemoShop.Web/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoShop.Web.ViewComponents
+{
+	public class SiteHeaderViewComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("SiteHeader");
+		}
+	}
+
+	public class SiteFooterViewComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("SiteFooter");
+		}
+
+
+	}
+}
