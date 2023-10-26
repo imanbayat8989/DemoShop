629cebe78cf39173901ab143abedd413d6dc709d
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-10-26 12:31:51 +0330

    10/26/2023 section 30 Add Ticket Properties

---
+++ DemoShop.DataLayer/Entities/Contacts/Ticket.cs
@@ -0,0 +1,75 @@
+﻿using DemoShop.DataLayer.Entities.Account;
+using DemoShop.DataLayer.Entities.common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DemoShop.DataLayer.Entities.Contacts
+{
+    public class Ticket : BaseEntities
+    {
+        #region properties
+
+        public long OwnerId { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Title { get; set; }
+
+        [Display(Name = "وضعیت تیکت")]
+        public TicketState TicketState { get; set; }
+
+        [Display(Name = "بخش مورد نظر")]
+        public TicketSection TicketSection { get; set; }
+
+        [Display(Name = "اولویت")]
+        public TicketPriority TicketPriority { get; set; }
+
+        public bool IsReadByOwner { get; set; }
+
+        public bool IsReadByAdmin { get; set; }
+
+        #endregion
+
+        #region relations
+
+        public User Owner { get; set; }
+
+        #endregion
+    }
+
+    public enum TicketSection
+    {
+        [Display(Name = "پشتیبانی")]
+        Support,
+        [Display(Name = "فنی")]
+        Technical,
+        [Display(Name = "آموزشی")]
+        Academic
+    }
+
+    public enum TicketPriority
+    {
+        [Display(Name = "کم")]
+        Low,
+        [Display(Name = "متوسط")]
+        Medium,
+        [Display(Name = "زیاد")]
+        High
+    }
+
+    public enum TicketState
+    {
+        [Display(Name = "در حال بررسی")]
+        UnderProgress,
+        [Display(Name = "پاسخ داده شده")]
+        Answered,
+        [Display(Name = "بسته شده")]
+        Closed
+    }
+}
---
+++ DemoShop.DataLayer/Entities/Contacts/TicketMessage.cs
@@ -0,0 +1,35 @@
+﻿using DemoShop.DataLayer.Entities.Account;
+using DemoShop.DataLayer.Entities.common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DemoShop.DataLayer.Entities.Contacts
+{
+    public class TicketMessage : BaseEntities
+    {
+        #region properties
+
+        public long TicketId { get; set; }
+
+        public long SenderId { get; set; }
+
+        [Display(Name = "متن پیام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Text { get; set; }
+
+        #endregion
+
+        #region relations
+
+        public Ticket Ticket { get; set; }
+
+        public User Sender { get; set; }
+
+        #endregion
+    }
+}
