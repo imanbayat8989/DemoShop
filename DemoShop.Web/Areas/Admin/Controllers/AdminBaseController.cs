a1014102ba4f075e6acb0b9f2bd8bd25f4694084
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-10-29 12:36:51 +0330

    10/29/2023 section 46 add Admin Panel

---
+++ DemoShop.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoShop.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class AdminBaseController : Controller
+    {
+        protected string SuccessMessage = "SuccessMessage";
+        protected string WarningMessage = "WarningMessage";
+        protected string InfoMessage = "InfoMessage";
+        protected string ErrorMessage = "ErrorMessage";
+    }
+}
---
+++ DemoShop.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoShop.Web.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        #region index
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
