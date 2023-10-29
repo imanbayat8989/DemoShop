3d8eb8217027e069f81282a6d64619d301789816
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-10-29 16:38:23 +0330

    10/29/2023 section 47 Fillter seller request for Admin panel

---
+++ DemoShop.Web/Areas/Admin/Controllers/SellerController.cs
@@ -0,0 +1,29 @@
+﻿using DemoShop.Application.Interface;
+using DemoShop.DataLayer.DTO.Seller;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoShop.Web.Areas.Admin.Controllers
+{
+    public class SellerController : AdminBaseController
+    {
+        #region constructor
+
+        private readonly ISellerService _sellerService;
+
+        public SellerController(ISellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        #endregion
+
+        #region seller requests
+
+        public async Task<IActionResult> SellerRequests(FilterSellerDTO filter)
+        {
+            return View(await _sellerService.FilterSellers(filter));
+        }
+
+        #endregion
+    }
+}
