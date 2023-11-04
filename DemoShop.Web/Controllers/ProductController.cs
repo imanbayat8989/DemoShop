3ee4042e19529ba350a0057e5cf0cc913d0d6834
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-11-04 09:51:33 +0330

    11/04/2023 section 76 Filter Products (section one)

---
+++ DemoShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using DemoShop.Application.Interface;
+using DemoShop.DataLayer.DTO.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoShop.Web.Controllers
+{
+    public class ProductController : SiteBaseController
+    {
+        #region constructor
+
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        #endregion
+
+        #region filter products
+
+        [HttpGet("products")]
+        public async Task<IActionResult> FilterProducts(FilterProductDTO filter)
+        {
+            var products = await _productService.FilterProducts(filter);
+
+            return View(products);
+        }
+
+        #endregion
+    }
+}
